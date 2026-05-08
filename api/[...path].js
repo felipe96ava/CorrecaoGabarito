fb9e5cc6a001529c83143637374626ae064f6bb9
Felipe <felipe96.ava@gmail.com>
2026-05-08 11:36:38 -0300

    Implementado reset de senha

--- api/[...path].js
+++
@@ -1,7 +0,0 @@
-import { dispatchApiRequest } from '../lib/api/dispatch.js';
-
-export const config = { api: { bodyParser: false } };
-
-export default function handler(req, res) {
-  return dispatchApiRequest(req, res);
-}
